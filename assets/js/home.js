21410f0431d802675ea40d5d7939c795c8fecd31
Chris Ameter <29186822+ameter@users.noreply.github.com>
2023-05-17 14:41:01 -0600

    make current page active in navbar

---
+++ assets/js/home.js
@@ -0,0 +1,14 @@
+/*!
+ * Start Bootstrap - Agency v5.2.2 (https://startbootstrap.com/template-overviews/agency)
+ * Copyright 2013-2019 Start Bootstrap
+ * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
+ */
+
+!function (e) {
+    "use strict";
+
+    // activate bootstrap scrollspy plugin to set the current section of the home page active in the navbar
+    e("body").scrollspy({
+        target: "#mainNav", offset: 56
+    });
+}(jQuery);
